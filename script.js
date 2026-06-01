ce65b7b7e844a3090481e39a2a4a7a428062c9b2
Meghana Rajoju <meghanarajoju20@gmail.com>
2026-06-01 14:49:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+const display = document.getElementById("display");
+const laps = document.getElementById("laps");
+
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let formattedHH = hh.toString().padStart(2, "0");
+  let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+
+  return `${formattedHH}:${formattedMM}:${formattedSS}`;
+}
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    display.textContent = timeToString(elapsedTime);
+  }, 100);
+}
+
+function pause() {
+  clearInterval(timerInterval);
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  display.textContent = "00:00:00";
+  elapsedTime = 0;
+  laps.innerHTML = "";
+}
+
+function lap() {
+  let li = document.createElement("li");
+  li.textContent = display.textContent;
+  laps.appendChild(li);
+}
+
+document.getElementById("start").addEventListener("click", start);
+document.getElementById("pause").addEventListener("click", pause);
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("lap").addEventListener("click", lap);
